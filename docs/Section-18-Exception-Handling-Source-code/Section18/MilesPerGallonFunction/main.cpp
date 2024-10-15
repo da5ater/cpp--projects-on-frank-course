7384c7367c9fb074e1a4631b1af46557b142ccbb
Mohamed <107060533+da5ater@users.noreply.github.com>
2024-10-15 22:45:27 +0300

    exception handling session 2
    next is the challenge

--- docs/Section-18-Exception-Handling-Source-code/Section18/MilesPerGallonFunction/main.cpp
+++ docs/Section-18-Exception-Handling-Source-code/Section18/MilesPerGallonFunction/main.cpp
@@ -5,6 +5,9 @@
 double calculate_mpg(int miles, int gallons) {
   if (gallons == 0)
     throw 0;
+
+  if (gallons < 0 || miles < 0)
+    throw std::string("negative");
   return static_cast<double>(miles) / gallons;
 }
 
@@ -22,6 +25,8 @@ int main() {
     std::cout << "Result: " << miles_per_gallon << std::endl;
   } catch (int &ex) {
     std::cerr << "Tried to divide by zero" << std::endl;
+  } catch (std::string &exx) {
+    std::cout << "negative";
   }
   std::cout << "Bye" << std::endl;
 
