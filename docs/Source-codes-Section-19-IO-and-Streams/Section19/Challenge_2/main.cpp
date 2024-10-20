ea4062e0e8984b5b53be40e831b24f6d6220bdcc
Mohamed <107060533+da5ater@users.noreply.github.com>
2024-10-21 00:29:08 +0300

    fininshed 3d challenge in the i\o stream section

--- docs/Source-codes-Section-19-IO-and-Streams/Section19/Challenge_2/main.cpp
+++ docs/Source-codes-Section-19-IO-and-Streams/Section19/Challenge_2/main.cpp
@@ -1,14 +1,49 @@
 // Section 19
 // Challenge 2
 // Automated Grader
+#include <fstream>
+#include <iomanip>
 #include <iostream>
-
 using namespace std;
 
-int main() {
-    
-    
-    cout << "Hello world" << endl;
-    return 0;
+int calc_degree(string test, string sol) {
+  int degree = 0;
+  for (size_t i = 0; i < sol.size(); i++) {
+    if (sol[i] == test[i])
+      degree++;
+  }
+  return degree;
 }
 
+int main() {
+  ifstream in_file{"responses.txt"};
+  string line;
+
+  if (!in_file) {
+    std::cerr << "error/n";
+    return 1;
+  }
+
+  string sol;
+  getline(in_file, sol);
+
+  cout << std::setw(10) << "student" << std::setw(10) << right << "mark"
+       << endl;
+
+  cout << "----------------------\n";
+  int count = 0;
+  string stud;
+  string test;
+  while (!in_file.eof()) {
+    if (count % 2 == 0) {
+      in_file >> stud;
+      cout << setw(10) << left << stud;
+    } else {
+      in_file >> test;
+      cout << setw(10) << right << calc_degree(test, sol) << endl;
+    }
+    count++;
+  }
+
+  return 0;
+}
