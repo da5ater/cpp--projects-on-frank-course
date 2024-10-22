e79db8d1fc6b6c7d0830c11c33988473ed480fe0
Mohamed <107060533+da5ater@users.noreply.github.com>
2024-10-22 22:12:02 +0300

    finished the i\o stream section
    2 more sections to go . YAY!!!

--- docs/Source-codes-Section-19-IO-and-Streams/Section19/Challenge_3/main.cpp
+++ docs/Source-codes-Section-19-IO-and-Streams/Section19/Challenge_3/main.cpp
@@ -1,14 +1,43 @@
 // Section 19
 // Challenge 3
 // Word counter
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
 int main() {
-    
-    
-    cout << endl;
-    return 0;
-}
 
+  ifstream in_file{"romeoandjuliet.txt"};
+  string line;
+  string buf;
+  string cmprthis;
+  int doccount;
+  int wrdcount;
+
+  cout << "write a word\n";
+  cin >> cmprthis;
+
+  if (!in_file) {
+    cerr << "error\n";
+    return 1;
+  }
+
+  while (!in_file.eof()) {
+    getline(in_file, line);
+    stringstream obj{line};
+
+    while (obj >> buf) {
+      doccount++;
+      if (buf.find(cmprthis) != string::npos) {
+        wrdcount++;
+      }
+    }
+  }
+  cout << "search " << doccount << "times \n";
+  cout << cmprthis << " was found " << wrdcount << "times\n";
+
+  cout << endl;
+  return 0;
+}
--- docs/Source-codes-Section-19-IO-and-Streams/Section19/Challenge_4/main.cpp
+++ docs/Source-codes-Section-19-IO-and-Streams/Section19/Challenge_4/main.cpp
@@ -1,10 +1,36 @@
 // Section 19
 // Challenge 4
 // Copy Romeo and Juliet with line numbers
+#include <fstream>
+#include <iomanip>
 #include <iostream>
+using namespace std;
 
 int main() {
-    
-    return 0;
-}
 
+  ifstream in_file{"romeoandjuliet.txt"};
+  ofstream out_file{"copy"};
+
+  if (!in_file) {
+    cerr << "error in input\n";
+    return 1;
+  }
+
+  if (!out_file) {
+    cerr << "error in output file \n";
+    return 1;
+  }
+
+  string line;
+  int count = 0;
+
+  while (getline(in_file, line)) {
+    if (!line.empty()) {
+      count++;
+      out_file << std::setw(10) << left << to_string(count) << right << line
+               << endl;
+    }
+  }
+
+  return 0;
+}
